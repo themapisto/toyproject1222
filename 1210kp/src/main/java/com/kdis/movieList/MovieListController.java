2b87ce5c4725aae939cc2a42833dbf6787780dac
김소연 <@DESKTOP-JRE4A3N>
2021-03-06 16:15:56 +0900

    주간/주말 오피스 추가 중복 데이터 방지 수정 중

--- 1210kp/src/main/java/com/kdis/movieList/MovieListController.java
+++ 1210kp/src/main/java/com/kdis/movieList/MovieListController.java
@@ -34,15 +34,13 @@ public class MovieListController {
 	@Inject
 	private MovieListService service;
 
-	
-    MovieVO vo = new MovieVO();
+	MovieVO vo = new MovieVO();
 
 	@Inject
 	private SqlSession sqlSession;
 
-
-	@RequestMapping(value = "/movieMain", method = RequestMethod.GET)
-	public String getApi() throws Exception {
+	@RequestMapping(value = "/dailyBoxOf", method = RequestMethod.GET)
+	public String list(Model model) throws Exception {
 
 		String REQUEST_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
 		String AUTH_KEY = "0a49df840ef75532829c0570cee0b18b";
@@ -55,8 +53,8 @@ public class MovieListController {
 
 		paramMap.put("key", AUTH_KEY);
 		paramMap.put("targetDt", DATE_FMT.format(cal.getTime()));
-		paramMap.put("itemPerPage", "10");
-		paramMap.put("multiMovieYn", "Y");
+		paramMap.put("itemPerPage", "0");
+		paramMap.put("multiMovieYn", "N");
 		paramMap.put("repNationCd", "K");
 
 		StringBuilder sb = new StringBuilder();
@@ -75,6 +73,7 @@ public class MovieListController {
 			System.out.println(requestURL);
 			HttpURLConnection conn = (HttpURLConnection) requestURL.openConnection();
 
+			Date now = new Date();
 			conn.setRequestMethod("GET");
 			conn.setDoInput(true);
 
@@ -95,26 +94,26 @@ public class MovieListController {
 			Iterator<Object> iter = dailyBoxOfficeList.iterator();
 			System.out.println(dailyBoxOfficeList);
 			while (iter.hasNext()) {
-				
-				//기존 데이터와 비교해서 중복되지 않도록 처리 
-				
-				
+
+				// 기존 데이터와 비교해서 중복되지 않도록 처리
+
 				JSONObject boxOffice = (JSONObject) iter.next();
 				System.out.print("??" + boxOffice.get("movieNm"));
-
+			
 				vo.setMovieCd(boxOffice.get("movieCd").toString());
 				vo.setMovieNm(boxOffice.get("movieNm").toString());
 				vo.setRank(boxOffice.get("rank").toString());
 				vo.setOpenDt(boxOffice.get("openDt").toString());
 				vo.setAudiAcc(boxOffice.get("audiAcc").toString());
 				vo.setSalesAmt(boxOffice.get("salesAmt").toString());
-			
-				System.out.print("??" + vo.getAudiAcc()+"???"+vo.getMovieNm()+"sddd"+vo.getMovieCd()+"???"+vo.getRank());
+				// System.out.println(DATE_FMT);
+
+				System.out.print("??" + vo.getAudiAcc() + "???" + vo.getMovieNm() + "sddd" + vo.getMovieCd() + "???"
+						+ vo.getRank());
 				sqlSession.insert("movieInsert", vo);
-				
+
 				System.out.printf("{순위:%s, 제목:%s, 개봉일:%s, 누적관객수:%s}\n", boxOffice.get("rank"), boxOffice.get("movieNm"),
 						boxOffice.get("openDt"), boxOffice.get("audiAcc"));
-
 
 			}
 
@@ -122,22 +121,105 @@ public class MovieListController {
 
 			e.printStackTrace();
 		}
-		 return "/movie/movieMain";
-	}
-	
-	@RequestMapping(value = "/dailyBoxOf", method = RequestMethod.GET)
-	public String list(Model model) throws Exception {
-		
+
 		model.addAttribute("list", service.list());
-		System.out.println("dddd"+service.list());
+		System.out.println("dddd" + service.list());
+
 		return "/movie/dailyBoxOf";
-	 }
-	 
-    
-	 @RequestMapping(value = "/weeklyBoxOf")
-	 public String weeklyBoxOf() throws Exception {
-		 return "/movie/weeklyBoxOf";
-	 }
-	 
+	}
+
+	@RequestMapping(value = "/weeklyBoxOf")
+	public String weeklyList(Model model) throws Exception {
+
+		String REQUEST_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json";
+		String AUTH_KEY = "0a49df840ef75532829c0570cee0b18b";
+		SimpleDateFormat DATE_FMT = new SimpleDateFormat("yyyyMMdd");
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.DATE, -1);
+
+		Map<String, String> paramMap = new HashMap<String, String>();
+
+		paramMap.put("key", AUTH_KEY);
+		paramMap.put("targetDt", DATE_FMT.format(cal.getTime()));
+		paramMap.put("itemPerPage", "0");
+		paramMap.put("itemPerPage", "10");
+		paramMap.put("multiMovieYn", "N");
+		paramMap.put("repNationCd", "K");
+
+		StringBuilder sb = new StringBuilder();
+
+		for (String mapkey : paramMap.keySet()) {
+			System.out.println("Key:" + mapkey + ", Value:" + paramMap.get(mapkey));
+
+			sb.append("&");
+			sb.append(mapkey).append('=').append(paramMap.get(mapkey));
+
+		}
+
+		try {
+
+			URL requestURL = new URL(REQUEST_URL + "?" + sb.toString());
+			System.out.println(requestURL);
+			HttpURLConnection conn = (HttpURLConnection) requestURL.openConnection();
+
+			Date now = new Date();
+			conn.setRequestMethod("GET");
+			conn.setDoInput(true);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			String readline = null;
+			StringBuffer response = new StringBuffer();
+			while ((readline = br.readLine()) != null) {
+				response.append(readline);
+			}
+
+			System.out.println(response);
+			JSONObject responseBody = new JSONObject(response.toString());
+			System.out.println(responseBody);
+			JSONObject boxOfficeResult = responseBody.getJSONObject("boxOfficeResult");
+			String boxofficeType = boxOfficeResult.getString("boxofficeType");
+			System.out.println(boxofficeType);
+			JSONArray dailyBoxOfficeList = boxOfficeResult.getJSONArray("dailyBoxOfficeList");
+			Iterator<Object> iter = dailyBoxOfficeList.iterator();
+			System.out.println(dailyBoxOfficeList);
+			while (iter.hasNext()) {
+
+				// 기존 데이터와 비교해서 중복되지 않도록 처리
+
+				JSONObject boxOffice = (JSONObject) iter.next();
+				System.out.print("??" + boxOffice.get("movieNm"));			
+				vo.setMovieCd(boxOffice.get("movieCd").toString());
+				vo.setMovieNm(boxOffice.get("movieNm").toString());
+				vo.setRank(boxOffice.get("rank").toString());
+				vo.setOpenDt(boxOffice.get("openDt").toString());
+				vo.setAudiAcc(boxOffice.get("audiAcc").toString());
+				vo.setSalesAmt(boxOffice.get("salesAmt").toString());
+				// System.out.println(DATE_FMT);
+
+				System.out.print("??" + vo.getAudiAcc() + "???" + vo.getMovieNm() + "sddd" + vo.getMovieCd() + "???"
+						+ vo.getRank());
+				sqlSession.insert("movieInsert", vo);
+
+				System.out.printf("{순위:%s, 제목:%s, 개봉일:%s, 누적관객수:%s}\n", boxOffice.get("rank"), boxOffice.get("movieNm"),
+						boxOffice.get("openDt"), boxOffice.get("audiAcc"));
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		model.addAttribute("list", service.list());
+		System.out.println("dddd" + service.list());
+		return "/movie/weeklyBoxOf";
+	}
+
+	@RequestMapping(value = "/movieMain")
+	public String movieMain() throws Exception {
+		return "/movie/movieMain";
+
+	}
 
 }
